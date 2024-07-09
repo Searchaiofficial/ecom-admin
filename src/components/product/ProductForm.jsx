27ef79a248e7e6cd1a38a97d9a53a039ac3b1be8
Harsh Kumar <harshkumar878983@gmail.com>
2024-07-09 19:41:26 +0530

    removed required field from mantainence

--- src/components/product/ProductForm.jsx
+++ src/components/product/ProductForm.jsx
@@ -1845,9 +1845,7 @@ function ProductForm() {
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-orange-600 ">
                   <textarea
                     type="text"
-                    {...register("maintainanceDetails", {
-                      required: "maintainance Description is required",
-                    })}
+                    {...register("maintainanceDetails")}
                     id="maintainanceDetails"
                     className="block flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                   />
